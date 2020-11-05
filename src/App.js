95305708c21a641706e3a4e35f9b047be8cfcc56
marshall405 <marshall.slemp@gmail.com>
2020-11-05 17:21:16 -0600

    adds ability to add links as prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
 
 import NavBar from './components/NavBar'
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar logo="Custom NavBar" links={['about', 'contact', 'projects']} />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function NavBar() {
+
+const header = {
+    margin: '0',
+    padding: '10px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+}
+const ul = {
+    display: 'flex',
+    listStyle: 'none'
+}
+const li = {
+    margin: '0 10px',
+}
+function NavBar({ logo, links }) {
     return (
-        <div>
-            Navbar
-        </div>
+        <header style={header}>
+            <h1 className="logo">{logo}</h1>
+            <nav>
+                <ul style={ul} className="nav-links">
+                    {
+                        links.map(link => {
+                            return (
+                                <li style={li}><a href={`/${link}`}> {link} </a></li>
+                            )
+                        })
+                    }
+                </ul>
+            </nav>
+        </header>
     )
 }
+
+NavBar.propTypes = {
+    links: PropTypes.array,
+    logo: PropTypes.string.isRequired
+}
+
+
+export default NavBar;
